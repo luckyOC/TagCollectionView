698977a1dd307183375419cb498ade661bd5cdca
小小 <lucky@xiaoxiaodembp.lan>
2016-04-06 17:08:15 +0800

    Initial Commit

---
+++ TagCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TagCollectionView
+//
+//  Created by 小小 on 16/4/6.
+//  Copyright © 2016年 小小. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
