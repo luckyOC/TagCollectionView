62678b2f8852796efefce5aa2b9f0e9d3f9ef589
小小 <lucky@xiaoxiaodeMBP.lan>
2016-04-06 17:31:11 +0800

    first commit

---
+++ TagCollectionView/TagCollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  TagCollectionViewLayout.swift
+//  TagCollectionView
+//
+//  Created by 小小 on 16/4/6.
+//  Copyright © 2016年 小小. All rights reserved.
+//
+
+import UIKit
+
+class TagCollectionViewLayout: UICollectionViewLayout {
+
+}
